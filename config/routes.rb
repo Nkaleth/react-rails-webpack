d238c067cb9024ec846ba2bd343712ce160cd3c3
Nkaleth <nseguralu@gmail.com>
2023-06-05 19:20:15 -0500

    ADD method random in order to get a random message from the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'root/index'
+  namespace :api do
+    namespace :v1 do
+      get '/random_message', to: 'messages#random'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
